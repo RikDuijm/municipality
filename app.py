c482dc90c81f63f1df595ff8351e8f7c29b939be
RikDuijm <rikduijm@gmail.com>
2019-11-04 17:18:19 +0000

    minor changes app.py to try to deplore on Heroku

--- app.py
+++ app.py
@@ -170,4 +170,5 @@ def street(streetname):
 
 if __name__ == '__main__':
     app.secret_key = 'geheimpje'
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
