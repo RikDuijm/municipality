9a699602f3b26f9b4a49c9aa2d80097e6786f062
RikDuijm <rikduijm@gmail.com>
2019-11-07 15:50:31 +0000

    cleaned code

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for, session
-from flask_pymongo import PyMongo # Needed to connect Flask to the MongoDB 
+from flask_pymongo import PyMongo # Needed to connect Flask to the MongoDB
 from bson.objectid import ObjectId # Convert in Bson-bject to retrieve record in MongoDB by report ID
-from bson import Binary
-import re
+from bson import Binary # Needed to upload and retrieve files.
+import re # Needed for search functionality
 import bcrypt #https://pypi.org/project/bcrypt/
-# Libraries necesary to store smaller binary files in
+''' Libraries necesary to store smaller binary files in '''
 import uuid
 from bson.binary import Binary, UUIDLegacy, STANDARD
 from bson.codec_options import CodecOptions
 
 app = Flask(__name__)
 
+# Point Heroku to the config variable (MONGO_URI) to keep the production database connection string secret.
 app.config["MONGO_DBNAME"] = 'municipality'
 app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
-
 
 # Create an instance of PyMongo. Add the app into that with a constructor method.
 mongo = PyMongo(app)
 
 """
 - Make connection to the database
-- render a template (reports.html)
-- supply the reports collection (reports=mongo.db.reports,), which will be returned from making a call directly to Mongo.
-- with the find() method, which will return everything.
+- render a template
 """
-
 @app.route('/')
 @app.route('/index')
 def index():
@@ -48,14 +45,15 @@ def search():
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
+    """Provides logic for search bar"""
     if request.method == 'POST':
         users = mongo.db.users
         existing_user = users.find_one({'username' : request.form['username']})
-
+         # check if username already exists.
         if existing_user is None:
             salt = bcrypt.gensalt()
             hashed = bcrypt.hashpw(request.form['password'].encode('utf-8'), salt)
-
+            # safe username and password in MongoDB and log in immediately
             users.insert_one({'username': request.form['username'], 'password' : hashed})
             session['username'] = request.form['username']
             return redirect("add_report")
@@ -75,6 +73,7 @@ def login():
         if login_user:
             salt = request.form['password'].encode('utf-8')
             input_password = bcrypt.hashpw(salt, login_user['password'])
+            # check credentials and see if user is citizen or admin
             if input_password == login_user['password']:
                 session['username'] = request.form['username']
                 session['logged_in'] = True
@@ -82,6 +81,7 @@ def login():
                     return redirect("get_reports")
                 else:
                     return redirect("add_report")
+            # in case credentials are wrong
             else:
                 return redirect("login_error")
         else:
@@ -99,6 +99,8 @@ def logout():
 
 @app.route('/get_reports')
 def get_reports():
+    # supply the reports collection (reports=mongo.db.reports,), which will be returned from making a call directly to Mongo.
+    # with the find() method, which will return everything.
     return render_template("reports.html", reports=mongo.db.reports.find().sort('date', -1))
 
 @app.route('/add_report')
@@ -111,9 +113,9 @@ def insert_report():
     reports.insert_one(request.form.to_dict())
     return redirect("get_reports")
 
-
 @app.route('/edit_report/<report_id>')
 def edit_report(report_id):
+    # connecting with the correct report in the database
     the_report =  mongo.db.reports.find_one({"_id": ObjectId(report_id)})
     all_categories = mongo.db.categories.find()
     return render_template('editreport.html', report=the_report,
@@ -136,6 +138,7 @@ def update_report(report_id):
 @app.route('/delete_report/<report_id>')
 def delete_report(report_id):
     reports = mongo.db.reports
+    # connecting with the correct report in the database
     reports.remove({'_id' : ObjectId(report_id)})
     return redirect(url_for('get_reports'))
 
--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    /* set the date and time no right now */
     $(function() {
     $('#datetimepicker1').datetimepicker({
         format: 'L, LT',
@@ -9,7 +9,7 @@ $(document).ready(function () {
 
 });
 
-/* code used from: https://jsfiddle.net/taditdash/hDtA3/ */
+/* code used from: https://jsfiddle.net/taditdash/hDtA3/ to restrict use of spaces in username upon registering*/
 function AvoidSpace(event) {
     var k = event ? event.which : window.event.keyCode;
     if (k == 32) return false;
