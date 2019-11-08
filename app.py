65ece1e57d8dacb690b5c540b57359d063cde9d5
RikDuijm <rikduijm@gmail.com>
2019-11-08 09:17:56 +0000

    changed debug to False

--- app.py
+++ app.py
@@ -145,4 +145,4 @@ def delete_report(report_id):
 if __name__ == '__main__':
     app.secret_key = os.getenv('SECRET_KEY')
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=False)
